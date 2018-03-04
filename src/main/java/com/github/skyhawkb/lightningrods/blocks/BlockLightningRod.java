00f3db459a6b37fa1b2badfb8bd9b2cb4bc0b20c
SkyHawkB <danielmey626@gmail.com>
2018-03-03 22:23:50 -0500

    added an AABB to the Lightning Rod

--- src/main/java/com/github/skyhawkb/lightningrods/blocks/BlockLightningRod.java
+++ src/main/java/com/github/skyhawkb/lightningrods/blocks/BlockLightningRod.java
@@ -1,17 +1,16 @@
 package com.github.skyhawkb.lightningrods.blocks;
 
 import com.github.skyhawkb.lightningrods.LightningRods;
-import jline.internal.Nullable;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.IBlockAccess;
@@ -79,4 +78,8 @@ public class BlockLightningRod extends Block {
     * TODO: add an AABB to this block
     * TODO: make it need a solid block beneath (at all times)
     */
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return new AxisAlignedBB(0.25D, 0.0D, 0.25D, 0.75D, 0.875D, 0.75D);
+    }
 }
