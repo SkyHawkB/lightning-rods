6062c7ae5f3b7a808be017dbb1c056b450083527
SkyHawkB <danielmey626@gmail.com>
2018-03-03 22:01:27 -0500

    add a ton of shit just for 1 block, bump version

--- src/main/java/com/github/skyhawkb/lightningrods/CommonProxy.java
+++ src/main/java/com/github/skyhawkb/lightningrods/CommonProxy.java
@@ -1,4 +1,7 @@
 package com.github.skyhawkb.lightningrods;
 
+import net.minecraft.item.Item;
+
 public class CommonProxy {
+    public void registerItemRenderer(Item item, int meta, String id) {}
 }
--- src/main/java/com/github/skyhawkb/lightningrods/LightningRods.java
+++ src/main/java/com/github/skyhawkb/lightningrods/LightningRods.java
@@ -1,13 +1,17 @@
 package com.github.skyhawkb.lightningrods;
 
+import com.github.skyhawkb.lightningrods.blocks.ModBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.event.RegistryEvent;
 
 @Mod(modid = LightningRods.MODID, name = LightningRods.NAME, version = LightningRods.VERSION)
@@ -19,6 +23,9 @@ public class LightningRods {
     @Mod.Instance(MODID)
     public static LightningRods instance;
 
+    @SidedProxy(clientSide = "com.github.skyhawkb.lightningrods.ClientProxy", serverSide = "com.github.skyhawkb.lightningrods.CommonProxy")
+    public static CommonProxy proxy;
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {}
     @Mod.EventHandler
@@ -29,12 +36,23 @@ public class LightningRods {
     @Mod.EventBusSubscriber
     public static class RegistrationHandler {
        @SubscribeEvent
-       public static void registerItems(RegistryEvent.Register<Item> event) {
-
+       public static void registerItems(RegistryEvent.Register<Item> e) {
+           ModBlocks.registerItems(e.getRegistry());
        }
        @SubscribeEvent
-       public static void registerBlocks(RegistryEvent.Register<Block> event) {
-
+       public static void registerBlocks(RegistryEvent.Register<Block> e) {
+           ModBlocks.registerBlocks(e.getRegistry());
+       }
+       @SubscribeEvent
+       public static void registerModels(ModelRegistryEvent e) {
+           ModBlocks.registerModels();
        }
     }
+
+    public static final CreativeTabs MOD_TAB = new CreativeTabs("lightningrods") {
+        @Override
+        public ItemStack getTabIconItem() {
+            return new ItemStack(ModBlocks.LIGHTNING_ROD);
+        }
+    };
 }
